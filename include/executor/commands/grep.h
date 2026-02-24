b0d6c647df0ac5cec59847ec79a03e2dd4caf8e1
domosedy <ilayvin05@mail.ru>
2026-02-24 22:46:42 +0300

    [docs] added documentation and comments to grep file

--- include/executor/commands/grep.h
+++ include/executor/commands/grep.h
@@ -9,6 +9,17 @@
 
 namespace btft::interpreter::executor::commands {
 
+/**
+ * @brief Command implementation for the 'grep' utility - searches for patterns
+ * in text
+ *
+ * This class implements the ICommand interface to provide grep-like
+ * functionality for searching text patterns. It inherits from ICommand and is
+ * designed to be used in a command execution pipeline.
+ *
+ * The class follows the Factory Method pattern with CreateCommand() to allow
+ * dynamic creation of command instances.
+ */
 class GrepCommand final : public ICommand {
 public:
     GrepCommand() = default;
